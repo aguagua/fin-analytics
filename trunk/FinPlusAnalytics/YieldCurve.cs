9f6841ffd8c8d9ccef7c1ce9afa0c7032c4406c3
jon.norton@fin-plus.co.uk <jon.norton@fin-plus.co.uk@c08f2546-080c-3ebc-a9f5-6450adc29b8a>
2015-01-19 09:46:22 +0000

    Transfer concepts of markets and trade chances from C++ QLProxy

--- trunk/FinPlusAnalytics/YieldCurve.cs
+++ trunk/FinPlusAnalytics/YieldCurve.cs
@@ -6,8 +6,34 @@ using System.Threading.Tasks;
 
 namespace FinPlusAnalytics
 {
-    public class YieldCurve
+    string YieldCurve(string marketName, string curveName, string discountCurveName, int settlementDate, string rates, string dayCount, double tolerance, string holidays)
     {
+	    try 
+	    {
+	        std::vector<std::string> ratesVector = stringToVector(rates);
+		    Calendar calendar = Holidays(holidays);
+		    Date settleDate(settlementDate);
+            settleDate = calendar.adjust(settleDate);
+		    if(tolerance == 0)	tolerance = 1.0e-15;
+		
+		    std::vector<boost::shared_ptr<RateHelper>> instruments;
 
-    }
+		    for each(std::string r in ratesVector)
+		    {
+			    boost::shared_ptr<RateHelper> rate = rateHelpers[marketName][r];
+			    if(rate) instruments.push_back(rate);
+		    }
+
+		    rateCurves[marketName][curveName] = boost::shared_ptr<YieldTermStructure>(new PiecewiseYieldCurve<Discount,LogLinear>(settleDate, instruments, DayCount(dayCount), tolerance));
+		
+		    SwapEngine(marketName, curveName, discountCurveName == "" ? curveName : discountCurveName);
+
+		    return curveName; 
+
+	    }
+	    catch (Exception e) 
+	    {
+		    return e.what();
+	    }
+    } 
 }
